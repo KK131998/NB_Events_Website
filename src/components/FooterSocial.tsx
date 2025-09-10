bff553e88f80edb69c6bb6a6129ad97cd363a98d
KlausKeisers <102816917+KlausKeisers@users.noreply.github.com>
2025-09-10 10:46:43 +0200

    Insatgram Link hinzugefügt

--- src/components/FooterSocial.tsx
+++ src/components/FooterSocial.tsx
@@ -7,13 +7,25 @@ export default function FooterSocial() {
         <div className={classes.footer}>
             <Container className={classes.inner}>
                 <Group gap={0} className={classes.links} justify="flex-end" wrap="nowrap">
+                    {/*
                     <ActionIcon size="lg" color="gray" variant="subtle">
                         <IconBrandTwitter size={18} stroke={1.5} />
                     </ActionIcon>
                     <ActionIcon size="lg" color="gray" variant="subtle">
                         <IconBrandYoutube size={18} stroke={1.5} />
                     </ActionIcon>
-                    <ActionIcon size="lg" color="gray" variant="subtle">
+                    */}
+                    <ActionIcon
+                        size="lg"
+                        color="gray"
+                        variant="subtle"
+                        component="a"
+                        href="https://www.instagram.com/_nbevents/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Instagram"
+                        title="Instagram"
+                    >
                         <IconBrandInstagram size={18} stroke={1.5} />
                     </ActionIcon>
                 </Group>
